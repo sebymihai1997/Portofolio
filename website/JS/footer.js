13269000b07cec8cb19013cd81c141750dd042b6
sebymihai1997 <46874724+sebymihai1997@users.noreply.github.com>
2026-06-01 22:50:30 +0300

    Add email icon to footer and enhance styling for social links

--- website/JS/footer.js
+++ website/JS/footer.js
@@ -19,7 +19,7 @@ function loadFooter(basePath) {
               </div>
               <div class="u-container-style u-layout-cell u-size-30 u-layout-cell-2">
                 <div class="u-container-layout u-valign-middle-sm u-valign-middle-xs u-container-layout-2">
-                  <p class="u-large-text u-text u-text-default u-text-variant u-text-2"> Connect with me</p>
+                  <p class="u-large-text u-text u-text-default u-text-variant u-text-2"> Connect with me</p>s
                   <div class="u-social-icons u-spacing-20 u-social-icons-1">
                     <a class="u-social-url" target="_blank" href="https://www.facebook.com/mer00s/" title="Facebook">
                       <span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1">
@@ -31,6 +31,20 @@ function loadFooter(basePath) {
                       <span class="u-icon u-icon-circle u-social-icon u-social-linkedin u-icon-2">
                         <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112.196 112.196"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-7ddf"></use></svg>
                         <svg class="u-svg-content" viewBox="0 0 112.196 112.196" id="svg-7ddf"><g><circle style="fill:#007AB9;" cx="56.098" cy="56.097" r="56.098"></circle><g><path style="fill:#F1F2F2;" d="M89.616,60.611v23.128H76.207V62.161c0-5.418-1.936-9.118-6.791-9.118    c-3.705,0-5.906,2.491-6.878,4.903c-0.353,0.862-0.444,2.059-0.444,3.268v22.524H48.684c0,0,0.18-36.546,0-40.329h13.411v5.715    c-0.027,0.045-0.065,0.089-0.089,0.132h0.089v-0.132c1.782-2.742,4.96-6.662,12.085-6.662    C83.002,42.462,89.616,48.226,89.616,60.611L89.616,60.611z M34.656,23.969c-4.587,0-7.588,3.011-7.588,6.967    c0,3.872,2.914,6.97,7.412,6.97h0.087c4.677,0,7.585-3.098,7.585-6.97C42.063,26.98,39.244,23.969,34.656,23.969L34.656,23.969z     M27.865,83.739H41.27V43.409H27.865V83.739z"></path></g></g></svg>
+                      </span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="mailto:mihai.sebastian.andrei@gmail.com" title="Email">
+                      <span class="u-icon u-icon-circle u-social-icon u-social-email u-icon-3">
+                        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 -31.5 256 256"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-gmail"></use></svg>
+                        <svg class="u-svg-content" viewBox="0 -31.5 256 256" id="svg-gmail">
+                          <g>
+                            <path d="M58.1818182,192.049515 L58.1818182,93.1404244 L27.5066233,65.0770089 L0,49.5040608 L0,174.59497 C0,184.253152 7.82545455,192.049515 17.4545455,192.049515 L58.1818182,192.049515 Z" fill="#4285F4"></path>
+                            <path d="M197.818182,192.049515 L238.545455,192.049515 C248.203636,192.049515 256,184.224061 256,174.59497 L256,49.5040608 L224.844415,67.3422767 L197.818182,93.1404244 L197.818182,192.049515 Z" fill="#34A853"></path>
+                            <polygon fill="#EA4335" points="58.1818182 93.1404244 54.0077618 54.4932827 58.1818182 17.5040608 128 69.8676972 197.818182 17.5040608 202.487488 52.4960089 197.818182 93.1404244 128 145.504061"></polygon>
+                            <path d="M197.818182,17.5040608 L197.818182,93.1404244 L256,49.5040608 L256,26.2313335 C256,4.64587897 231.36,-7.65957557 214.109091,5.28587897 L197.818182,17.5040608 Z" fill="#FBBC04"></path>
+                            <path d="M0,49.5040608 L26.7588051,69.5731646 L58.1818182,93.1404244 L58.1818182,17.5040608 L41.8909091,5.28587897 C24.6109091,-7.65957557 0,4.64587897 0,26.2313335 L0,49.5040608 Z" fill="#C5221F"></path>
+                          </g>
+                        </svg>
                       </span>
                     </a>
                   </div>
